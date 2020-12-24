ff2d3a3c9139532b297163adb2b84429ff2a609b
dadez <dadez@pm.me>
2020-12-24 15:48:12 +0100

    added test for healthz

--- pkg/healthz/healthz_test.go
+++ pkg/healthz/healthz_test.go
@@ -2,6 +2,7 @@ package healthz
 
 import (
 	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -21,4 +22,31 @@ func TestHealthzHandler(t *testing.T) {
 			HealthzHandler(tt.args.w, tt.args.r)
 		})
 	}
+	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
+	// pass 'nil' as the third parameter.
+	req, err := http.NewRequest("GET", "/healthz", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(HealthzHandler)
+
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+	// Check the response body is what we expect.
+	expected := "ok"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+
 }
